09d74458d3be8a195a1ac0a1f586497569cdcec2
ANSHIKA-26 <114569262+ANSHIKA-26@users.noreply.github.com>
2024-06-28 23:35:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,36 +33,5 @@ app.listen(PORT,() =>{
     console.log(`Server is running on port ${PORT}`);   
 })
 
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Replace with your email details
-const transporter = nodemailer.createTransport({
-  service: 'gmail', // Use your email provider
-  auth: {
-    user: 'arora.anshika.26@gmail.com', // Your email
-    pass: 'Gmail_Anshi26' // Your email password
-  }
-});
-
-app.post('/send', (req, res) => {
-  const { name, email, message } = req.body;
-  const mailOptions = {
-    from: email,
-    to: 'arora.anshika.26@gmail.com', // Your email
-    subject: `Message from ${name}`,
-    text: message
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.send('error'); // Or render an error page
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.send('sent'); // Or render a success page
-    }
-  });
-});
