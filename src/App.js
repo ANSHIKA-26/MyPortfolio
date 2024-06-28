11abf8d20f3d90c36e4e476df2d169a198e5dc67
ANSHIKA-26 <114569262+ANSHIKA-26@users.noreply.github.com>
2024-06-28 23:44:43 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout/Layout.js";
+import About from "./pages/About/About";
+import TechStack from "./pages/TechStack/TechStack.js";
+import Projects from "./pages/Projects/project.js";
+import Education from "./pages/Education/Education.js";
+import WorkExp from "./pages/workexp/WorkExp.js";
+import Contact from "./pages/Contact/Contact.js";
+import MobileNav from "./components/MobileNav/MobileNav.js";
+import ScrollToTop from "react-scroll-to-top";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <MobileNav />
+      <Layout />
+      <div className="container">
+        <About />
+        <TechStack />
+        <Projects />
+        <Education />
+        <WorkExp />
+        <Contact />
+
+      </div>
+
+      <ScrollToTop
+        smooth
+        color="#f29f67"
+        style={{ backgroundColor: "#1e1e2c", borderRadius: "80px" }}
+      />
+    </>
   );
 }
 
